8756baabb86e3d4df46a68b392f99b1127f50f3c
Richard Dominick <richdom2185@gmail.com>
2022-02-12 00:04:48 +0800

    cleanup

--- python/main.py
+++ python/main.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python
 
-# ev3devKit - ev3dev toolkit for LEGO MINDSTORMS EV3
-#
-# Copyright 2015,2017 David Lechner <david@lechnology.com>
-#           2015 Stefan Sauer <ensonic@google.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301, USA.
-
-# ui_demo.py - main function for running UI demo
-
 import gi
 import os
 
